337f12a168c2f5e39f6ccb8f1617c0296d76b002
Elghat <36163194+Elghat@users.noreply.github.com>
2023-02-05 00:57:37 +0100

    Trail Scene in child of character

---
+++ scenes/Trail/trail.gd
@@ -0,0 +1,14 @@
+extends Line2D
+
+export var trailLength = 50
+var point = Vector2()
+
+
+func _process(_delta):
+	global_position = Vector2(0,0)
+	global_rotation = 0
+	point = get_parent().global_position
+	
+	add_point(point)
+	while get_point_count() > trailLength:
+		remove_point(0)
