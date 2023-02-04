8b45477c5d29716c4bef9be471dab0e00b286875
teleteub <60780@etu.he2b.be>
2023-02-04 13:24:14 +0100

    mouvement character mise a jour

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-
-
 export var vel = Vector2()
 export var MAXSPEED = 200
 export var ACCELERATION = 25
@@ -9,11 +7,17 @@ export var DECELERATION = 25
 export var DEADZONE = 0.1
 var currentSpeed = Vector2(0,0)
 
+var connected_joypads = Input.get_connected_joypads()
+export var gamePadId = 0
+onready var currentDevice = connected_joypads[gamePadId]
+
+
+
 func _physics_process(_delta):
 	if Input.is_action_pressed("ui_left") && Input.get_axis("ui_left","ui_right") < DEADZONE:
-		vel.x =Input.get_axis("ui_left","ui_right")
+		vel.x =Input.get_joy_axis(gamePadId, JOY_AXIS_0)
 	elif Input.is_action_pressed("ui_right") && Input.get_axis("ui_left","ui_right") > (DEADZONE *- 1):
-		vel.x =Input.get_axis("ui_left","ui_right")
+		vel.x =Input.get_joy_axis(gamePadId, JOY_AXIS_0)
 	else :
 		if currentSpeed.x > 0 :
 			currentSpeed.x -= DECELERATION
@@ -25,9 +29,9 @@ func _physics_process(_delta):
 				currentSpeed.x = 0
 			
 	if Input.is_action_pressed("ui_up") && Input.get_axis("ui_up","ui_down") < DEADZONE:
-		vel.y = Input.get_axis("ui_up","ui_down")
+		vel.y = Input.get_joy_axis(gamePadId, JOY_AXIS_1)
 	elif Input.is_action_pressed("ui_down") && Input.get_axis("ui_up","ui_down") > (DEADZONE*- 1):
-		vel.y = Input.get_axis("ui_up", "ui_down")
+		vel.y = Input.get_joy_axis(gamePadId, JOY_AXIS_1)
 	else:
 		if currentSpeed.y > 0 :
 			currentSpeed.y -= DECELERATION
@@ -49,7 +53,6 @@ func _physics_process(_delta):
 			currentSpeed.y = MAXSPEED
 		elif currentSpeed.y < -MAXSPEED:
 			currentSpeed.y = -MAXSPEED
-	print(currentSpeed)
 	
 	translate(currentSpeed)
 
